f3d0d0be8706a15545593169ab4a1e981cf84a10
酒井邦也 <k_sakai@waku-2.com>
2019-03-10 14:07:41 +0900

    add a submodule

--- Pods/Target Support Files/CTCheckbox/CTCheckbox-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CTCheckbox : NSObject
-@end
-@implementation PodsDummy_CTCheckbox
-@end
--- Pods/Target Support Files/SwiftCop/SwiftCop-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftCop : NSObject
-@end
-@implementation PodsDummy_SwiftCop
-@end
